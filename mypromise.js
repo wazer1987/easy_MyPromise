bfc20d83ccaf37fc39ac32edce746c6b24c16c83
北城韩雨 <1052101962@qq.com>
2021-01-24 13:56:15 +0800

    then机制 all方法

--- mypromise.js
+++ mypromise.js
@@ -57,8 +57,37 @@ MyPromise.prototype = {
                 return MyPromise.reject(result)
             }
         }
-        this.resolveFunction = resolveFunction
-        this.rejectFunction = rejectFunction
+
+    //16. 这里为了能当状态改变成功或者失败之后 我们then 里面什么也不传  然后在在第二个then中调用 能拿到结果 所以我们还是要返回一个promise实例 
+    //这样才能接着用then调用 为了保证this 指向正确我们缓存一下
+        var _this = this
+        return new MyPromise(function(resolve,reject) {
+            //17.这里由于我们不知道 究竟调用的是 成功的函数  还是 失败的函数 所以 这我们在把函数包装一层方便监控
+            _this.resolveFunction = function(result){
+                //18.这里有两种情况 第一种是 你自己有返回值 比如10 20 一个对象之类的 不是一个promise的成功的状态 我们直接返回就可以
+                // 第二种 就是 你代码写的问题 直接报错了 所以我们要返回失败的状态
+                try{
+                    var x = resolveFunction(result)
+                    // 判断 你是返回的promise实例 还是特殊的值
+                    x instanceof MyPromise? x.then(resolve,reject):resolve(x)
+                }catch(e){
+                    reject(e)
+                }
+            }
+            _this.rejectFunction = function(reason) {
+                try{
+                    var x = rejectFunction(reason)
+                    // 判断 你是返回的promise实例 还是特殊的值
+                    x instanceof MyPromise? x.then(resolve,reject):resolve(x)
+                }catch(e){
+                    reject(e)
+                }
+            }
+        })
+    },
+    catch:function(rejectFunction){
+        // xxx.catch(() => {}) => xxx.then(null,() => {})
+        return this.then(null,rejectFunction)
     }
 }
 //15.我们promise被当作对象的时候 还有两个方法 一个是 promise.resolve 和 promise.reject 这两个方法都会返回一个成功的promise实例
@@ -72,12 +101,67 @@ MyPromise.reject = function reject (reason) {
         reject(reason)
     })
 }
+//19. Promise.all([fn1(),fn2(),fn3(),10]) 返回的也是promise实例 每个参数都是一个promise的实例 
+//只要有一个promise实例 是失败的 那么返回的整个状态就是失败
+//最后返回实例的顺序 也是按照我们上面传递参数的顺序是一样的
+MyPromise.all = function(promiseArr) {
+    //1. 先缓存下this
+    var _this = this
+    //2. 最后返回的也是一个promise的实例数组 值是有所有单个实例组成的数组
+    return new MyPromise(function(resolve,reject){
+        var index = 0 //用来计数
+            results = [] //用来存放我们每次promise实例的结果
+        //5. 这里我们还需要写一个通知的函数 当我们已经循环完了所有你传进来的数组 并且结果都已经返回了 就我们就该把结果 拿出去了
+        var fire = function () {
+            if(index >= promiseArr.length){
+                resolve(results)
+            }
+        }
+        for(var i = 0; i < promiseArr.length; i++){
+            (function(i){
+            //3. 这个是每个实例
+            var item = promiseArr[i]
+
+            //5. 这里开始判断 如果你不是promise实例 那么我们就直接返还给你 并且结束当前 开始下一轮循环
+            if(!(item instanceof MyPromise)){
+                results[i] = item
+                index++
+                //看看 循环结束了么 如果结束了 我们就要返回结果了
+                fire()
+                return
+            }
+            //6. 如果不是promise实例 我们就要拿到它的返回结果 放进results
+            item.then(function(res){
+                results[i] = res
+                index++
+                fire()
+            }).catch(function(reason){
+                reject(reason)
+            })
+            })(i)
+            //8.以上那么写 是会有错的 因为 你then 里面是异步的 这个时候循环已经结束了 i 的值 已经变成最后循环那轮结束的了 所以我们需要用闭包包起来 保证i的值是当前的
+        }
+    })
+}
 
 
-var p1 = new MyPromise((resolve,reject) => {
-    resolve('ok')
+function fn1(){
+    return MyPromise.resolve(10)
+}
+function fn2(){
+    return MyPromise.resolve(20)
+}
+function fn3(){
+    return new MyPromise( function (resolve,reject) {
+        setTimeout(() => {
+            resolve(30)
+        },100)
+    })
+}
+const p1 = MyPromise.all([fn1(),fn2(),fn3(),40])
+console.log(p1)
+p1.then(res => {
+    console.log(`${res}`)
+}).catch(e => {
+    console.log(e)
 })
-//目前作用域链机制还没洗完 所以还不能连续调用.then
-// p1.then(null).ten(() => {
-
-// })
